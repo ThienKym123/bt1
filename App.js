4140a7f01880839122f41850956e26a6ed464075
ThienKym123 <143048033+ThienKym123@users.noreply.github.com>
2024-06-17 22:21:48 +0700

    App.js

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 30,
+    paddingTop: 50,
     borderBottomWidth: 1,
   },
   headerText: {
